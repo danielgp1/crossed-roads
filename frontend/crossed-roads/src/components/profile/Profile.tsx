b8c0dbcb42aeba1b81d65f00f1575f7596b255c1
Daniel Petrov <dd01@abv.bg>
2023-07-13 19:30:27 +0300

    added profile view

---
+++ frontend/crossed-roads/src/components/profile/Profile.tsx
@@ -0,0 +1,40 @@
+import './Profile.css'
+import pic from './assets/john.jpeg'
+
+export default function Profile() {
+    return (
+        <div className="profile-grid">
+            <div className='profile-data'>
+                <div className='profile-pic-container'>
+                    <img className='profile-pic' alt='profile pic' src={pic} ></img>
+                </div>
+                <div className='profile-info'>
+                    <span className='names'>John Cena</span>
+                    <div className='other-info'>
+                        <div className="profile-field">
+                            <label className='field-label'>Username:</label>
+                            <span className='field-span'>johncena#1234</span>
+                        </div>
+                        <div className="profile-field">
+                            <label className='field-label'>Email:</label>
+                            <span className='field-span'>john@mail.com</span>
+                        </div>
+                        <div className="profile-field">
+                            <label className='field-label'>Birthday:</label>
+                            <span className='field-span'>1969-01-20</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='profile-buttons'>
+                <button className='profile-button'>Edit Profile Data</button>
+                <button className='profile-button'>Change Password</button>
+                <button className='profile-button'>Create a Post</button>
+                <button className='profile-button'>Manage Posts</button>
+            </div>
+            <div>
+                ok
+            </div>
+        </div>
+    )
+}
--- frontend/crossed-roads/src/components/service/main/ServiceMain.tsx
+++ frontend/crossed-roads/src/components/service/main/ServiceMain.tsx
@@ -1,7 +1,11 @@
+import Profile from '../../profile/Profile'
 import './ServiceMain.css'
 
 export default function ServiceMain() {
     return (
-        <div className='service-main-grid'>Service</div>
+        <div className='service-main-grid'>
+            <Profile />
+            <p>Cars</p>
+        </div>
     )
 }
