59a3272da0b22a8692918d685060781cff4062fb
Daniel Petrov <dd01@abv.bg>
2023-07-19 16:04:03 +0300

    minor changed in available colors

--- frontend/crossed-roads/src/components/available-colors/AvailableColors.tsx
+++ frontend/crossed-roads/src/components/available-colors/AvailableColors.tsx
@@ -8,26 +8,66 @@ export default function AvailableColors() {
                 <div className='available-colors-header'>Available Paintjobs</div>
             </div>
             <div className='available-colors-container-grid'>
-                <Car color='green' direction='yellow' />
-                <Car color='blue' direction='yellow' />
-                <Car color='black' direction='yellow' />
-                <Car color='red' direction='yellow' />
-                <Car color='maroon' direction='yellow' />
-                <Car color='white' direction='yellow' />
-                <Car color='purple' direction='yellow' />
-                <Car color='cyan' direction='yellow' />
-                <Car color='darkblue' direction='yellow' />
-                <Car color='yellow' direction='yellow' />
-                <Car color='green' direction='yellow' />
-                <Car color='blue' direction='yellow' />
-                <Car color='black' direction='yellow' />
-                <Car color='red' direction='yellow' />
-                <Car color='maroon' direction='yellow' />
-                <Car color='white' direction='yellow' />
-                <Car color='purple' direction='yellow' />
-                <Car color='cyan' direction='yellow' />
-                <Car color='darkblue' direction='yellow' />
-                <Car color='yellow' direction='yellow' />
+                <div className='available-color-container'>
+                    <Car color='green' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='blue' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='red' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='yellow' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='cyan' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='maroon' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='black' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='darkblue' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='white' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='purple' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='green' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='blue' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='red' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='yellow' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='cyan' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='maroon' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='black' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='darkblue' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='white' direction='#f9d71c' />
+                </div>
+                <div className='available-color-container'>
+                    <Car color='purple' direction='#f9d71c' />
+                </div>
             </div>
         </div>
     )
