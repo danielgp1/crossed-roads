d5721e21b9ca5f31284dbfb7113c3f5cde57a002
Daniel Petrov <dd01@abv.bg>
2023-07-21 20:14:04 +0300

    fixed profile pic functionality

--- frontend/crossed-roads/src/components/profile/Profile.tsx
+++ frontend/crossed-roads/src/components/profile/Profile.tsx
@@ -20,16 +20,19 @@ export default function Profile() {
     useEffect(() => {
         const userID = localStorage.getItem('userID');
         const authToken = localStorage.getItem('userToken');
-        if (userID) {
+        if (userID && authToken) {
             axios.get(`http://localhost:8080/api/users/${userID}`, {
                 headers: {
                     Authorization: `Bearer ${authToken}`,
                 },
             })
-                .then((response) => setUserData(response.data))
+                .then((response) => {
+                    setUserData(response.data);
+                    setImageURL(response.data.profile_pic_url);
+                })
                 .catch((error) => console.error('Error fetching user data:', error));
         }
-    }, [setUserData]);
+    }, []);
 
     if (!userData) {
         return <div>Loading...</div>;
@@ -40,42 +43,74 @@ export default function Profile() {
     const handleProfilePicClick = () => {
         fileInputRef.current?.click();
     };
-    
-    const handleImageChange = () => {
+
+    const handleImageChange = async () => {
         const imageInput = document.querySelector("#image-input") as HTMLInputElement;
         const file = imageInput?.files?.[0];
-        if(file) {
-            uploadImageToS3(file);
+        if (file) {
+            const imageUrl = await uploadImageToS3(file);
+            if (imageUrl) {
+                updateUserProfilePicUrl(imageUrl);
+            }
         }
     }
 
     const uploadImageToS3 = async (file: File) => {
         try {
-          const bucketName = process.env.REACT_APP_BUCKET_NAME;
-          const key = `images/${file.name}`;
-    
-          const params = {
-            Bucket: bucketName,
-            Key: key,
-            Body: file,
-          };
-    
-          const data = await s3.send(new PutObjectCommand(params));
-          setImageURL(`https://${process.env.REACT_APP_BUCKET_NAME}.s3.${process.env.REACT_APP_REGION}.amazonaws.com/${key}`);
-          console.log("Image uploaded successfully:", data);
+            const bucketName = process.env.REACT_APP_BUCKET_NAME;
+            const key = `images/${file.name}`;
+
+            const params = {
+                Bucket: bucketName,
+                Key: key,
+                Body: file,
+            };
+
+            const data = await s3.send(new PutObjectCommand(params));
+            console.log("Image uploaded successfully:", data);
+
+            const imageUrl = `https://${process.env.REACT_APP_BUCKET_NAME}.s3.${process.env.REACT_APP_REGION}.amazonaws.com/${key}`;
+            setImageURL(imageUrl);
+            return imageUrl;
+
         } catch (error) {
-          console.error("Error uploading image:", error);
+            console.error("Error uploading image:", error);
+            return null;
         }
-      };
+    };
+
+    const updateUserProfilePicUrl = async (imageUrl: string | null) => {
+        if (imageUrl) {
+            const authToken = localStorage.getItem('userToken');
+            const userID = localStorage.getItem('userID');
+            if (authToken && userID) {
+                try {
+                    await axios.put(
+                        `http://localhost:8080/api/users/${userID}`,
+                        { profile_pic_url: imageUrl },
+                        {
+                            headers: {
+                                Authorization: `Bearer ${authToken}`,
+                            },
+                        }
+                    );
+                    console.log("Profile picture URL updated successfully");
+                } catch (error) {
+                    console.error("Error updating profile picture URL:", error);
+                }
+            }
+        }
+    };
+
 
     return (
         <div className="profile-grid">
             <div className='profile-data'>
                 <div className='profile-pic-container'>
-                    <img 
+                    <img
                         className='profile-pic'
-                        alt='profile pic' 
-                        src={imageURL ? imageURL : default_pic} 
+                        alt='profile pic'
+                        src={imageURL ? imageURL : default_pic}
                         onClick={handleProfilePicClick} />
                     <input
                         id='image-input'
