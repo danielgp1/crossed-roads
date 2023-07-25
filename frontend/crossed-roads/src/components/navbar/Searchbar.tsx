2e325727e3e2eda2f1ac8a8bc3ca288c28fc98f1
Daniel Petrov <dd01@abv.bg>
2023-07-25 14:07:45 +0300

    added searchbar results styles

--- frontend/crossed-roads/src/components/navbar/Searchbar.tsx
+++ frontend/crossed-roads/src/components/navbar/Searchbar.tsx
@@ -1,13 +1,15 @@
 import './Searchbar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
+import def from '../assets/default_pic.png'
 
 interface User {
-    id:number;
-    first_name:string;
-    last_name:string;
+    id: number;
+    first_name: string;
+    last_name: string;
+    profile_pic_url: string;
 }
 
 export default function Searchbar() {
@@ -16,26 +18,31 @@ export default function Searchbar() {
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const value: string = event.target.value;
-        const newValue = value.replace('#', "\%23");
+        const newValue = value.replace('#', "%23");
         setSearchQuery(value);
         performSearch(newValue);
     };
 
     const performSearch = (username: string) => {
+        if (username.trim() === '') {
+            setSearchResults([]);
+            return;
+        }
         const authToken = localStorage.getItem('userToken');
 
         axios
-        .get(`http://localhost:8080/api/users/search?username=${username}`, {
-            headers: {
-                Authorization: `Bearer ${authToken}`,
-              },
-        })
-        .then((response) => {
-          setSearchResults(response.data);
-        })
-        .catch((error) => {
-          console.error('Error performing search:', error);
-        });
+            .get(`http://localhost:8080/api/users/search?username=${username}`, {
+                headers: {
+                    Authorization: `Bearer ${authToken}`,
+                },
+            })
+            .then((response) => {
+                const filteredResults = response.data.filter((user: User) => user.id !== parseInt(localStorage.getItem("userID")!));
+                setSearchResults(filteredResults);
+            })
+            .catch((error) => {
+                console.error('Error performing search:', error);
+            });
 
     };
 
@@ -50,13 +57,23 @@ export default function Searchbar() {
                 onChange={handleInputChange}
             >
             </input>
-            <ul>
-                {searchResults.map((user) => (
-                    <li key={user.id}>
-                        {user.first_name} {user.last_name}
-                    </li>
-                ))}
-            </ul>
+            {searchQuery !== '' && (
+                <div className='search-results'>
+                    {searchResults.length > 0 ? (
+                        searchResults.map((user) => (
+                            <div key={user.id} className="result-item">
+                                <img className="result-profile-pic" src={user.profile_pic_url ?? def} alt="Profile Pic" />
+                                <div className="result-user-info">
+                                    <span className="result-user-name">{user.first_name} {user.last_name}</span>
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <div className="no-results">No Results Found</div>
+                    )}
+                </div>
+            )}
+
         </div>
     )
 }
